f16734444229060406d7029f28ad578891d46e36
Mattisbbn <mattisbbn@gmail.com>
2024-10-03 12:04:25 +0200

    Second commit

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php 
 require_once("partials/head.php");
-require_once("partials/header.php");
+// require_once("partials/header.php");
 
 ?>
 <body>
     <main>
 <?php
 require_once("pages/login_page/login_page.php");
+
 ?>
     </main>
 </body>
+ 
 </html>
--- pages/login_page/login_page.php
+++ pages/login_page/login_page.php
@@ -1,18 +1,23 @@
-<section>
-
-
-
-
-<form action="">
-    <label for="email_input">Email</label>
-<input id="email_input" type="email">
-<label for="password_input">Mot de passe</label>
-<input id="password_input" type="password">
-
-
-</form>
-
-
-
-
+<section id="login_section" class="d-flex justify-content-center align-items-center">
+    
+        <h1 id="login_title">Webtraining</h1><!-- Choisissez la largeur souhaitée -->
+        <form id="login_form" action="">
+            <div id="login_choice">
+                <Button>Animateur</Button>
+                <div id="login_separation"></div>
+                <Button>Étudiant</Button>
+            </div>
+            <div id="login_container">
+                <label for="email_input">Email</label>
+                <input id="email_input" type="email"  placeholder="Email" required>
+          
+         
+                <label for="password_input">Mot de passe</label>
+                <input id="password_input" type="password" placeholder="Mot de passe" required>
+            </div>
+            <button id="submit_login" type="submit" >Se connecter</button>
+        </form>
+  
+    
 </section>
+
