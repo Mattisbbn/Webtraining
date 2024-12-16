0b5f1f13592bf7c871b47a71e248bef750d409e3
Mattisbbn <mattisbbn@gmail.com>
2024-12-16 13:28:45 +0100

    Update

--- model/changepassword.php
+++ model/changepassword.php
@@ -1,4 +1,5 @@
 <?php
+require_once("controller/database.php");
 class changePassword{
 
     private object $pdo;
@@ -17,18 +18,28 @@ class changePassword{
     }
 
     public function changePasswordByEmail($email,$hashedPassword,$token){
-        $sql = "UPDATE users SET password = :password WHERE email = :email";
-      
+        
+        if($this->checkRecoveryStatus($token)){
         try{
+            $sql = "UPDATE users SET password = :password WHERE email = :email";
             $stmt = $this->pdo->prepare($sql);
             $stmt->bindParam(':password', $hashedPassword);
             $stmt->bindParam(':email', $email);
             $stmt->execute();
             $this->changeRecoveryStatus($token);
-            echo("Le mot de passe à été changé avec succés");
+            echo("<script>
+            setTimeout(function(){
+            window.location.href = 'login';
+        }, 3000);
+        </script>");
+
+        echo("Le mot de passe à bien été changé, redirection dans 3 secondes");
         } catch(Exception $e){
             echo("Le mot de passe n'a pas été changé <br>");
             echo($e->getMessage());
+        }
+        }else{
+            echo("le mot de passe à déja été changé");
         }
     }
 
@@ -38,4 +49,35 @@ class changePassword{
         $stmt->bindParam(':token', $token);
         $stmt->execute();
     }
+
+    private function checkRecoveryStatus($token){
+        $sql ="SELECT recovery.status FROM recovery WHERE token = :token";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':token', $token);
+        $stmt->execute();
+      
+        if($stmt->fetchColumn() === "finished"){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+
+if(isset($_GET["token"])){
+    $token = $_GET["token"];
+    $changePasswordModel = new changePassword($pdo);
+    $tokenAccount = $changePasswordModel->fetchRecoveryByToken($token);
+
+    if($tokenAccount){
+        require_once("view/changepassword/form/changepasswordform.php");
+    }else{
+        echo("Le token n'est pas valide");
+    }
+}
+
+if(isset($_POST["new-password"])){
+    $hashedPassword = password_hash($_POST["new-password"], PASSWORD_DEFAULT);
+    $email = $tokenAccount['email'];
+    $changePasswordModel->changePasswordByEmail($email,$hashedPassword,$token);
 }
--- view/changepassword/changepassword.php
+++ view/changepassword/changepassword.php
@@ -1,17 +1,1 @@
-<?php 
-require_once("controller/database.php");
-require_once("model/changepassword.php");
-$changePasswordModel = new changePassword($pdo);
-
-if(isset($_GET["token"])){
-    $token = $_GET["token"];
-
-    $tokenAccount = $changePasswordModel->fetchRecoveryByToken($token);
-
-    if($tokenAccount){
-        require_once("view/changepassword/form/changepasswordform.php");
-    }else{
-        echo("Le token n'est pas valide");
-    }
-}
-?>
+<?php require_once("model/changepassword.php") ?>
